19a7d90d1fd5313a38e1fb4f7cee6220313637f3
Dahlia <nuradzainala@gmail.com>
2022-05-01 23:57:14 +0800

    fixing few things

--- recipes.h
+++ recipes.h
@@ -7,6 +7,60 @@
  ALMANSARI AWS ABDULRAHMAN AHMED           | CS01080941     | 03A
  *****************************************************************************************************************************/
 #include <iostream>
+#include <iomanip> 
 #include "potion.h"
 
 using namespace std;
+
+class Bloom {
+    protected:
+        char size;
+        int peach, rose, vanilla, musk, sandalwood;
+
+        virtual void display() = 0;
+    public:
+        Bloom(char s, int pea, int rs, int van, int mu, int sw) {
+        size = s;
+        peach = pea;
+        rose = rs;
+        vanilla = van;
+        musk = mu;
+        sandalwood = sw;
+    }
+};
+
+class Shop: public Bloom {
+    public:
+
+        void display(){
+            cout << setw(43) << "***Welcome to our perfumery***"<<endl;
+            cout <<"---------------------------------------------------------\n";
+            cout << setw (37) << "Bloom Scents Menu"<<endl;
+            cout <<"---------------------------------------------------------\n";
+            cout << "\t\tPeach\t\t RM5.00\n\t\tRose\t\t RM7.00\n\t\tVanilla\t\t RM3.70\n\t\tMusk\t\t RM2.40\n\t\tSandalwood\t RM2.70\n";
+            cout <<"---------------------------------------------------------\n";
+        }
+
+        /*void customize(int bottles, int scents) {
+            cout << "How many perfume you want to buy? " << endl;
+            cin >> bottles;
+            cout <<"---------------------------------------------------------\n";
+            while (bottles <= 30){
+                cout << "How many scents you want to buy? (MAXIMUM 3 scents)\n";
+                cin>>scents;
+                cout <<"---------------------------------------------------------\n";
+                    if(scents<=3){
+                        cout << "Make your own perfume...\nPeach\nRose\nVanilla\nMusk\nSandalwood\n";
+                        for(int i=0; i<scents; i++)
+                        {
+                        cin>>per[i];
+                        }
+                    cout <<"-----------------------------------------------------------------------\n";
+                        cout << "The size of the bootle..\nSmall(30ml)\nMedium(100)\nLarge(200)";
+                        cin>>size;
+                    }
+                    else
+                    cout << "Sorry we only can sell 30 bottles in a day";
+            }
+        }*/
+};
