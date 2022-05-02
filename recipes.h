1e9c461ceb740074e4e2634360933701a72dd8ba
Dahlia <100366087+dahliaz@users.noreply.github.com>
2022-05-02 19:20:27 +0800

    minor fixes again

--- recipes.h
+++ recipes.h
@@ -33,47 +33,82 @@ class Shop: public Bloom {
 	protected:
 		int bottles, scents, drops;
 		char unique, size;
-    public:
+    	public:
 
-        void display(){
-            cout << setw(43) << "***Welcome to our perfumery***"<<endl;
-            cout <<"---------------------------------------------------------\n";
-            cout << setw (37) << "Bloom Scents Menu"<<endl;
-            cout <<"---------------------------------------------------------\n";
-        }
+        	void display(){
+            	cout << setw(43) << "***Welcome to our perfumery***"<<endl;
+            	cout <<"---------------------------------------------------------\n";
+            	cout << setw (37) << "Bloom Scents Menu"<<endl;
+            	cout <<"---------------------------------------------------------\n";
+        	}
         
-        int bottleChoice() {
-        	while(1){
-        		cout << "How many perfume you want to buy? " << endl;
-	            cin >> bottles;
-	            cout <<"---------------------------------------------------------\n";
-	            if (bottles <= 30)
-	            	return bottles;
-	            else
-	            	cout << "Sorry we only can sell 30 bottles in a day";
+        	int bottleChoice() {
+        		while(1){
+        			cout << "How many perfume you want to buy? " << endl;
+	        	    	cin >> bottles;
+	        	    	cout <<"---------------------------------------------------------\n";
+	            		if (bottles <= 30)
+	            			return bottles;
+	            		else
+	            			cout << "Sorry we only can sell 30 bottles in a day";
 			}   
 		}
 		
-	char uniqueChoice(){
-		while(1){
-			cout << "Do you want every bottle to be unique? (Y/N)" << endl;
-			cin >> uniqueChoice;
-			if (unique == 'Y' || unique == 'N')
-				return unique;
-			else
-				cout << "Please Enter A Valid Value" << endl;
+		char uniqueChoice(){
+			while(1){
+				cout << "Do you want every bottle to be unique? (Y/N)" << endl;
+				cin >> uniqueChoice;
+				if (unique == 'Y' || unique == 'N')
+					return unique;
+				else
+					cout << "Please Enter A Valid Value" << endl;
 			}
 		}
 		
-	int scentChoice() {
-		unique = uniqueChoice();
-		if (unique == 'Y'){
-			for(int i = 0; i < bottles; i++){
-				cout << "Bottle " << i + 1 << endl;
+		int scentChoice() {
+			unique = uniqueChoice();
+			if (unique == 'Y'){
+				for(int i = 0; i < bottles; i++){
+					cout << "Bottle " << i + 1 << endl;
+					scents = scentAmount();
+	                		cout <<"---------------------------------------------------------\n";
+                    			scentMenu();
+                    			for(int i = 0; i < scents; i++){ 
+						cout << "Enter the number for the scent you desire: ";
+						cin >> drops;
+                				switch (drops) {
+							case 1: 
+								peach += 1;
+								break;
+							case 2:
+								rose += 1;
+								break;
+							case 3:  
+								vanilla += 1;
+								break;
+							case 4:
+								musk += 1;
+								break;
+							case 5:
+								sandalwood += 1;
+								break;
+							default:
+								cout << "Invalid input";
+								if (i == 0)
+									i = 0;
+								else 
+									i -= 1;
+								break;
+						}
+                    			}
+	            		}
+			}
+			else if (unique == 'N'){
+				cout << "All " << bottles << "bottles" << endl;
 				scents = scentAmount();
-	                	cout <<"---------------------------------------------------------\n";
-                    		scentMenu();
-                    		for(int i = 0; i < scents; i++){ 
+                		cout <<"---------------------------------------------------------\n";
+                		scentMenu();
+                		for(int i = 0; i < scents; i++){ 
 					cout << "Enter the number for the scent you desire: ";
 					cin >> drops;
                 			switch (drops) {
@@ -99,85 +134,50 @@ class Shop: public Bloom {
 							else 
 								i -= 1;
 							break;
-					}
-                    		}
-	            	}
-		}
-		else if (unique == 'N'){
-			cout << "All " << bottles << "bottles" << endl;
-			scents = scentAmount();
-                	cout <<"---------------------------------------------------------\n";
-                	scentMenu();
-                	for(int i = 0; i < scents; i++){ 
-				cout << "Enter the number for the scent you desire: ";
-				cin >> drops;
-                		switch (drops) {
-					case 1: 
-						peach += 1;
-						break;
-					case 2:
-						rose += 1;
-						break;
-					case 3:  
-						vanilla += 1;
-						break;
-					case 4:
-						musk += 1;
-						break;
-					case 5:
-						sandalwood += 1;
-						break;
-					default:
-						cout << "Invalid input";
-						if (i == 0)
-							i = 0;
-						else 
-							i -= 1;
-						break;
-					}
-                		}
+						
+                			}
+				}
 			}
 		}
+		void scentMenu(){
+			cout << "Make your own perfume...\n\n";
+        		cout << "+-----+------------+---------+\n";
+			cout << "| No. | Scent      | Price   |\n";
+			cout << "+-----+------------+---------+\n";
+			cout << "| 1   | Peach      | RM 5.00 |\n";
+			cout << "+-----+------------+---------+\n";
+			cout << "| 2   | Rose       | RM 7.00 |\n";
+			cout << "+-----+------------+---------+\n";
+			cout << "| 3   | Vanilla    | RM 3.70 |\n";
+			cout << "+-----+------------+---------+\n";
+			cout << "| 4   | Musk       | RM 2.40 |\n";
+			cout << "+-----+------------+---------+\n";
+			cout << "| 5   | Sandalwood | RM 2.70 |\n";
+			cout << "+-----+------------+---------+\n";
+		}
 		
-	void scentMenu(){
-		cout << "Make your own perfume...\n\n";
-        	cout << "+-----+------------+---------+\n";
-		cout << "| No. | Scent      | Price   |\n";
-		cout << "+-----+------------+---------+\n";
-		cout << "| 1   | Peach      | RM 5.00 |\n";
-		cout << "+-----+------------+---------+\n";
-		cout << "| 2   | Rose       | RM 7.00 |\n";
-		cout << "+-----+------------+---------+\n";
-		cout << "| 3   | Vanilla    | RM 3.70 |\n";
-		cout << "+-----+------------+---------+\n";
-		cout << "| 4   | Musk       | RM 2.40 |\n";
-		cout << "+-----+------------+---------+\n";
-		cout << "| 5   | Sandalwood | RM 2.70 |\n";
-		cout << "+-----+------------+---------+\n";
-	}
-		
-	int scentAmount(){
-		while(1){
-			cout << "How many scents you want to buy? (MAXIMUM 3 scents)\n";
-	        	cin >> scents;
-	        	if (scents <= 3)
-	        		return scents;
-	        	else
-	        		cout << "Please Enter A Scent Amount Less Than 3" << endl;
+		int scentAmount(){
+			while(1){
+				cout << "How many scents you want to buy? (MAXIMUM 3 scents)\n";
+	        		cin >> scents;
+	        		if (scents <= 3)
+	        			return scents;
+	        		else
+	        			cout << "Please Enter A Scent Amount Less Than 3" << endl;
+			}
+			
 		}
 			
-	}
-			
-	char sizeChoice() {
-		while(1){
-			cout << "-----------------------------------------------------------------------\n";
-                	cout << "The size of the bootle..\nS: Small(30ml)\nM: Medium(100)\nL: Large(200)";
-                	cin >> size;
-                	if (size == 'S' || size == 'M' || size == 'L')
-                		return size;
-                	else
-                		cout << "Please Enter A Valid Size"
-            	}
-	}
+		char sizeChoice() {
+			while(1){
+				cout << "-----------------------------------------------------------------------\n";
+                		cout << "The size of the bootle..\nS: Small(30ml)\nM: Medium(100)\nL: Large(200)";
+                		cin >> size;
+                		if (size == 'S' || size == 'M' || size == 'L')
+                			return size;
+                		else
+                			cout << "Please Enter A Valid Size"
+            		}
+		}
 
 };
