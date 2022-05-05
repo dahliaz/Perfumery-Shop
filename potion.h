c89844e1862fcec65e5b8e4f19c7f07bbc7d7dce
AisyaAzley <104480197+AisyaAzley@users.noreply.github.com>
2022-05-05 20:15:09 +0800

    Update potion.h

--- potion.h
+++ potion.h
@@ -14,23 +14,19 @@ using namespace std;
 
 //WARNING! U CAN SIMPLIFY THIS INTO JUST 2 CLASSES PAYMENT & MEMBERCHECK
 
-class Payment{ 
-private:
-    char member; // <-- int -> char 
-    /*char option = 'o'; <-- total unecessary*/
-    double base = 1.5, sizePrice, total;
-
-public:
-    Member(){} // <-- i don't see a class called Member unless u mean MemberCheck 
+class MemberCheck: public Perfume {
+ private:
+ char m; // <-- int -> char 
+ 
+ public:
+ MemberCheck(){} // <-- i don't see a class called Member unless u mean MemberCheck 
     Member(int m, char opt){
         member = m;
     }
-
-};
-
-
-class MemberCheck: public Payment {
+ 
     // func Member() and no need to input request if constructor is expecting an input from main
+ 
+ void Member(void){
     do{
         switch(member)
     case 'Y':
@@ -42,12 +38,20 @@ class MemberCheck: public Payment {
     default:
         cout << "Invalid character is entered.Please try again." << endl;
         }while (member != 'Y' || member != 'N');//loop to repeat once invalid char is entered
+ }
 
         //end switch to choose member status
 };
 
-class Amount: public Payment, public Perfume {
-// func sizeCost()
+class Payment:public Perfume, public MemberCheck{ 
+private:
+    
+    /*char option = 'o'; <-- total unecessary*/
+    double base = 1.5, sizePrice=0.0, total=0.0;
+
+public:
+ 
+ void sizeCost(){
     if(size == 'S')
         sizePrice = 25.00;
     else if(size == 'M')
@@ -55,20 +59,28 @@ class Amount: public Payment, public Perfume {
     else if(size == 'L')
         sizePrice = 125.00;
 //price assigned to size
+ }
 
-//func totalCalc() & i've just embedded the prices into the formula 
+void totalCalc(){ //& i've just embedded the prices into the formula 
 total = (5*peach) + (7*rose) + (3.7*vanilla) + (2.4*musk) + (2.7*sandalwood) + base; //price including scents calculated
 
 total += sizePrice; //price including size calculated
 
 total *= bottles; //total price of one bottle multiplied by how many bottles bought
+ 
+ }
 
-//func Discount()
-    if(m = 'Y')
+double Discount(){
+    if(member = 'Y')
         return Total*0.9;
     else
         return Total;
-
 //if member then discount applicable
+}              
+
 };
+
+
+
+
 
